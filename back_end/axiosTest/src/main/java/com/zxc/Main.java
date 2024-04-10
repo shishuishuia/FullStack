5982fca90fe300f40e3579d8d3942b4fa465d3ea
shuishui <2480287925@qq.com>
2024-04-10 16:49:03 +0800

    successfully lanched backend

--- back_end/axiosTest/src/main/java/com/zxc/Main.java
+++ back_end/axiosTest/src/main/java/com/zxc/Main.java
@@ -1,5 +1,8 @@
 package com.zxc;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author 晓梦之尘
  * more about author: www.shuishuia.cn
@@ -8,8 +11,11 @@ package com.zxc;
  * @date 2024/4/10 16:40
  * @Version 1.0
  */
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
         System.out.println("Hello world!");
     }
 }
---
+++ back_end/axiosTest/src/main/java/com/zxc/controller/hhController.java
@@ -0,0 +1,25 @@
+package com.zxc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author 晓梦之尘
+ * more about author: www.shuishuia.cn
+ * @ClassName hhController
+ * @Description TODO
+ * @date 2024/4/10 16:42
+ * @Version 1.0
+ */
+
+@Controller
+public class hhController {
+
+    @GetMapping("hh")
+    @ResponseBody
+    public String hh(){
+        return "nihaoshijie";
+    }
+
+}
